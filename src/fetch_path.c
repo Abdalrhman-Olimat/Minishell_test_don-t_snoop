dc5fff55a70405567c15fe551927dfae7dc863dd
AhmadHirzallah <ahmad.hirzallah.pro@gmail.com>
2025-05-25 14:47:19 +0300

    fetch the path variable now is working with split

--- src/fetch_path.c
+++ src/fetch_path.c
@@ -1,5 +1,35 @@
 #include "../includes/mini.h"
 
+static int is_path_var(char *entry)
+{
+	return (entry && ft_strncmp(entry, "PATH=", 5) == 0);
+}
+
+static char **split_path_value(char *entry)
+{
+	if (!entry)
+		return (NULL);
+	return (ft_split(entry + 5, ':'));
+}
+
+char **fetch_path(t_shell *shell, int i)
+{
+	char **paths;
+
+	while (shell->analyzing_data.envp[i])
+	{
+		if (is_path_var(shell->analyzing_data.envp[i]))
+		{
+			paths = split_path_value(shell->analyzing_data.envp[i]);
+			return (paths);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+
+/*
 char **fetch_path(t_shell *shell, int i)
 {
 	char **paths;
@@ -15,4 +45,4 @@ char **fetch_path(t_shell *shell, int i)
 	}
 	return (NULL);
 }
-
+*/
