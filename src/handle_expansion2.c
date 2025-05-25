d3427dab7c5587abd66aeb727da8b3b577a5bcdb
AhmadHirzallah <ahmad.hirzallah.pro@gmail.com>
2025-05-25 19:04:32 +0300

    added more for appending and copy suffix to handle more commands appending and make it works perfectly

--- src/handle_expansion2.c
+++ src/handle_expansion2.c
@@ -8,9 +8,34 @@ char **allocate_expanded_args(char **original, char **suffix, int skip)
 	original_len = get_2d_len(original);
 	suffix_len = get_2d_len(suffix) - skip;
 	return (realloc_2d_appending(original, 
-			original_len, original_len + suffix_len));
+			original_len, (original_len + suffix_len)));
 }
 
+static int copy_one_by_one(char **dst, char **src, int *dst_i, int *src_i)
+{
+	while (src[*src_i])
+	{
+		dst[*dst_i] = ft_strdup(src[*src_i]);
+		if (!dst[*dst_i])
+			return (0);
+		(*dst_i)++;
+		(*src_i)++;
+	}
+	dst[*dst_i] = NULL;
+	return (1);
+}
+
+int copy_suffix(char **args, char **suffix, int start_idx, int skip_idx)
+{
+	int i;
+	int j;
+
+	i = start_idx;
+	j = skip_idx;
+	return (copy_one_by_one(args, suffix, &i, &j));
+}
+
+/*
 int copy_suffix(char **args, char **suffix, int start_index, int skip)
 {
 	int i;
@@ -29,3 +54,4 @@ int copy_suffix(char **args, char **suffix, int start_index, int skip)
 	args[i] = NULL;
 	return (1);
 }
+*/
