219f5a78c1852a1d4a0020283d939084357cbbfc
AhmadHirzallah <ahmad.hirzallah.pro@gmail.com>
2025-05-26 12:06:01 +0300

    handle commands before pipe  and skipped  after it is a pipe for  count_cmds_tokens and it works now completely

--- src/count_cmds_tokens.c
+++ src/count_cmds_tokens.c
@@ -1,5 +1,26 @@
 #include "../includes/mini.h"
 
+static int count_and_skip(t_input **cursor, size_t *word_count)
+{
+	while (*cursor && (*cursor)->type != TYPE_PIPE)
+	{
+		if ((*cursor)->type == TYPE_WORD)
+			(*word_count)++;
+		*cursor = (*cursor)->next;
+	}
+	return (0);
+}
+
+size_t count_cmds_tokens(t_input *start_token)
+{
+	size_t count;
+	
+	count = 0;
+	count_and_skip(&start_token, &count);
+	return (count);
+}
+
+/*
 size_t	count_cmds_tokens(t_input *current_token)
 {
 	size_t	tokens_count;
@@ -13,3 +34,4 @@ size_t	count_cmds_tokens(t_input *current_token)
 	}
 	return (tokens_count);
 }
+*/
