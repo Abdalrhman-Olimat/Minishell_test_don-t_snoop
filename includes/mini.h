0968fe5ec6fc6b4f013f680cd68c7f84a45a433d
Abdalrhman_olimat <abdalrhmanolimat911@gmail.com>
2025-06-11 15:53:02 +0300

    cd and cd2

--- includes/mini.h
+++ includes/mini.h
@@ -6,7 +6,7 @@
 /*   By: aeleimat <aeleimat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 11:23:49 by aeleimat          #+#    #+#             */
-/*   Updated: 2025/05/30 17:20:02 by aeleimat         ###   ########.fr       */
+/*   Updated: 2025/06/11 15:49:08 by aeleimat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -384,5 +384,8 @@ void    cleanup_shell(t_shell *shell);
 int     ft_exit(char **args, t_shell *shell);
 int     ft_isnumber(const char *str);
 void    ft_exit_handler(t_shell *shell, char *message, char **error_msgs, int exit_code);
+
+char	*get_env(const char *name);
+int	set_env(const char *name, const char *value);
 
 #endif
